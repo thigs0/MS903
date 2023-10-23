d98c1c2c8f9d795b5281f159ec9346bd09d4848e
thigs0 <tthiagosantos38@gmai.com>
2023-10-23 16:13:49 -0300

    update

--- tarefa6.jl
+++ tarefa6.jl
@@ -3,17 +3,25 @@ using DataFrames
 using CSV
 using Plots
 
-df = CSV.File("./files/circulo.csv", header=false, delim=" ")
-df = DataFrame(df)
-u = df[:,2]
-v = df[:,3]
+df_treino = CSV.File("./files/circulo_treina", header=false, delim=" ")
+df_teste = CSV.File("./files/circulo_testa", header=false, delim=" ")
+df_treino = DataFrame(df_treino)
+df_teste = DataFrame(df_teste)
 
-function circ(p::Vector, R::Int)
+u = df_treino[:,2]
+v = df_treino[:,3]
+w = df_treino[:,4]
+
+function circ(x::Vector,C::Vector ,R::Int)
   #p é um vetor com duas componentes(x,y)
+ # é um vetor com o centro da esfera
   #R é o raio do circulo
+  # A função diz se temos um ponto dentro ou fora da circunferencia
   
-
-  
+  c = (x[1]-C[1])^2 + (x[2]-C[2])^2 - R^2
+  return atan(c+(pi/2))
 end
-scatter(u,v)
+
+
+scatter(u,v, group=w)
 savefig("teste.png")
